dc9d6126353cde8a245fd5ed92bec52a680576a6
vvhybe <vvhybe@gmail.com>
2025-12-07 18:45:06 +0100

    making nvim transparent when colorscheme reloaded

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,47 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Highlight groups to make transparent
+local groups = {
+  "Normal",
+  "NormalNC",
+  "NormalFloat",
+  "FloatBorder",
+  "FloatTitle",
+  "FloatFooter",
+  "SignColumn",
+  "EndOfBuffer",
+  "LineNr",
+  "CursorLineNr",
+  "StatusLine",
+  "StatusLineNC",
+  "TabLine",
+  "TabLineSel",
+  "TabLineFill",
+  "ColorColumn",
+  "FoldColumn",
+  "Cursor",
+  "CursorColumn",
+  "CursorLine",
+  "PMenu",
+  "PMenuSel",
+  "NvimTreeNormal",
+  "NvimTreeRootFolder",
+  "NvimTreeStatusLine",
+}
+
+local function make_transparent()
+  for _, g in ipairs(groups) do
+    vim.api.nvim_set_hl(0, g, { bg = "NONE" })
+  end
+  -- making sure EndOfBuffer doesn't draw weird background markers
+  vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "NONE", bg = "NONE" })
+end
+
+make_transparent()
+
+-- Re-apply automatically when the colorscheme is (re)loaded
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = make_transparent,
+})
--- lua/whybe/plugins/ui.lua
+++ lua/whybe/plugins/ui.lua
@@ -1,11 +1,3 @@
-function ColorMyPencils(color)
-  color = color or "rose-pine"
-  vim.cmd.colorscheme(color)
-
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
   {
     "folke/noice.nvim",
@@ -101,7 +93,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      ColorMyPencils("yorumi")
+      vim.cmd.colorscheme("yorumi")
     end,
   },
 
